544138d60d9a1af3a5075c9c4d84e108ff14f64a
DESKTOP-CFOMFKS\User <marius.pasiulevicius@gmail.com>
2022-01-19 16:05:12 +0200

    now can add all info about flights to csv

---
+++ helper.py
@@ -0,0 +1,49 @@
+from datetime import datetime, timedelta
+from time import strptime
+
+def find_year(direction, columns):
+    year = [col for col in columns if direction in col.text][0].text
+    return year.split()[-1]
+
+def url_formater(day=0):
+    depart_time = datetime.today() + timedelta(days=day)
+    return_time = depart_time + timedelta(days=7)
+
+    dep_weekday = depart_time.strftime("%a")
+    dep_month = depart_time.strftime("%b")
+    dep_month_day = depart_time.strftime("+%d").replace('+0', '+')
+    dep_year = depart_time.strftime("%Y")
+
+    ret_weekday = return_time.strftime("%a")
+    ret_month = return_time.strftime("%b")
+    ret_month_day = return_time.strftime("+%d").replace('+0', '+')
+    ret_year = return_time.strftime("%Y")
+
+    URL = f"https://www.fly540.com/flights/nairobi-to-mombasa?isoneway=0&currency=USD&depairportcode=NBO&arrvairportcode=MBA&date_from=" \
+          f"{dep_weekday}%2C{dep_month_day}+{dep_month}+{dep_year}" \
+          f"&date_to={ret_weekday}%2C{ret_month_day}+{ret_month}+{ret_year}&adult_no=1&children_no=0&infant_no=0&searchFlight=&change_flight="
+    return URL
+
+def time_creation(time, date, year):
+    time = time[:-2] + " " + time[-2:]
+    hours_and_mins = datetime.strftime(datetime.strptime(time, "%I:%M %p"), "%H:%M")
+    month_and_day = date.replace(",", "").split()
+    month = strptime(month_and_day[2],'%b').tm_mon
+    flight_time = datetime.now().replace(year=int(year), month=month, day=int(month_and_day[1]),
+                                      hour=int(hours_and_mins.split(":")[0]),
+                                      minute=int(hours_and_mins.split(":")[1]), second=0)
+    return flight_time.strftime("%a %b %d %X GTM %Y")
+
+def form_flight_data(flight_data, flight_year):
+    dep_fl_time = flight_data.find("td", {"data-title": "Departs"}).find(class_="fltime").text[1:-1]
+    dep_fl_date = flight_data.find("td", {"data-title": "Departs"}).find(class_="fldate").text
+
+    arr_fl_time = flight_data.find("td", {"data-title": "Arrives"}).find(class_="fltime").text[1:-1]
+    arr_fl_date = flight_data.find("td", {"data-title": "Arrives"}).find(class_="fldate").text
+
+    departure_airport = flight_data.find("td", {"data-title": "Departs"}).select("br")[0].next_sibling[1:4]
+    arrives_airport = flight_data.find("td", {"data-title": "Arrives"}).select("br")[0].next_sibling[1:4]
+    dep_time = time_creation(dep_fl_time, dep_fl_date, flight_year)
+    arr_time = time_creation(arr_fl_time, arr_fl_date, flight_year)
+
+    return [departure_airport, arrives_airport, dep_time, arr_time]
--- main.py
+++ main.py
@@ -1,31 +1,7 @@
 import requests
-from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from csv import writer
-
-
-def url_formater(day=0):
-    depart_time = datetime.today() + timedelta(days=day)
-    return_time = depart_time + timedelta(days=7)
-
-    dep_weekday = depart_time.strftime("%a")
-    dep_month = depart_time.strftime("%b")
-    dep_month_day = depart_time.strftime("+%d").replace('+0', '+')
-    dep_year = depart_time.strftime("%Y")
-
-    ret_weekday = return_time.strftime("%a")
-    ret_month = return_time.strftime("%b")
-    ret_month_day = return_time.strftime("+%d").replace('+0', '+')
-    ret_year = return_time.strftime("%Y")
-
-    URL = f"https://www.fly540.com/flights/nairobi-to-mombasa?isoneway=0&currency=USD&depairportcode=NBO&arrvairportcode=MBA&date_from=" \
-          f"{dep_weekday}%2C{dep_month_day}+{dep_month}+{dep_year}" \
-          f"&date_to={ret_weekday}%2C{ret_month_day}+{ret_month}+{ret_year}&adult_no=1&children_no=0&infant_no=0&searchFlight=&change_flight="
-    return URL
-
-def find_year(direction, columns):
-    year = [col for col in columns if direction in col.text][0].text
-    return year.split()[-1]
+from helper import find_year, url_formater, form_flight_data
 
 URL = url_formater(day=10)
 page = requests.get(URL)
@@ -33,8 +9,8 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 departs = soup.find(class_='fly5-depart').find_all(class_='fly5-result')
 fly_backs = soup.find(class_="fly5-return").find_all(class_='fly5-result')
-
 info_table = soup.find(class_="fly5-query").find_all(class_="col-5")
+
 departing_year = find_year("Departing", info_table)
 returning_year = find_year("Returning", info_table)
 #
@@ -44,17 +20,18 @@ with open('flights_data.csv', 'w', encoding='utf8', newline='') as fl:
               'inbound_departure_airport', 'inbound_arrival_airport', 'inbound_departure_time', 'inbound_arrival_time', 'total_price', 'taxes']
     the_writer.writerow(header)
 
-for depart in departs:
-    for fly_back in fly_backs:
-        print(depart.find("td", {"data-title": "Departs"}).find(class_="fltime").text, end="")
-        print(depart.find("td", {"data-title": "Departs"}).find(class_="fldate").text, end="")
-        departure_airport = depart.find("td", {"data-title": "Departs"}).select("br")[0].next_sibling
-        print(departure_airport[1:4], end="")
-        print(departing_year, end="")
-        print()
-        # print(depart.find(class_="flprice").text + " + " + fly_back.find(class_="flprice").text)
+    for depart in departs:
+        for fly_back in fly_backs:
+            flight_info = []
+            dep_flight = form_flight_data(depart, departing_year)
+            flight_info.extend(dep_flight)
+            ret_flight = form_flight_data(fly_back, returning_year)
+            flight_info.extend(ret_flight)
+            flight_price = float(depart.find(class_="flprice").text) + float(fly_back.find(class_="flprice").text)
+            flight_taxes = 12.0
+            tax = "{:.2f}".format(flight_taxes)
+            price =  "{:.2f}".format(flight_price)
+            flight_info.append(price)
+            flight_info.append(tax)
+            the_writer.writerow(flight_info)
 
-
-
-
-# print(departs)
